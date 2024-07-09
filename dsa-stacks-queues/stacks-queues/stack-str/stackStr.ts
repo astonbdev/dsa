a89cbe42c28a8bfa1f5ef1aa82b77bb823c4373c
Brian Aston <astonb.dev@gmail.com>
2024-07-08 17:14:53 -0700

    stack strings done

--- dsa-stacks-queues/stacks-queues/stack-str/stackStr.ts
+++ dsa-stacks-queues/stacks-queues/stack-str/stackStr.ts
@@ -1,30 +1,35 @@
 import { IndexError } from "../common/ll";
 
+import { LLStr } from "../common/ll";
+
 /** StackStr: can remove from the top or add to the top. */
 
 class StackStr {
+  list: LLStr;
 
   constructor(initial: string[] = []) {
+    this.list = new LLStr(initial);
   }
 
   /** push(val): add val to top. Returns undefined. */
   push(val: string): void {
+    this.list.push(val);
   }
 
   /** pop(): remove and return item from top.
    * Throws error if empty. */
   pop(): string {
-    return "x";
+    return this.list.pop();
   }
 
   /** peek(): return the value of first item. */
   peek():string {
-    return "x";
+    return this.list.getAt(this.list.length - 1);
   }
 
   /** isEmpty(): is the stack empty? */
   isEmpty():boolean {
-    return false;
+    return this.list.length === 0;
   }
 }
 
