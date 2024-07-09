6ce9492b42b5615c344d39cfb7e841f0258f9c63
Brian Aston <astonb.dev@gmail.com>
2024-07-09 10:37:29 -0700

    finished find

--- recursion/find/find_start.ts
+++ recursion/find/find_start.ts
@@ -1,7 +1,18 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  //base case, array is empty, return false;
+  //test if current last value is the thing we want
+  //if it's not, pop it, and call recursively with new array
+  //if it is, return true;
+
+  if(arr.length === 0) return false;
+
+  if(arr[length-1] === val) return true;
+
+  arr.pop();
+
+  return find(arr, val);
 }
 
 export { find };
