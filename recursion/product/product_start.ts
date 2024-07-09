272b0368d6da40eac58ada305b40e29f5471a272
Brian Aston <astonb.dev@gmail.com>
2024-07-09 10:37:18 -0700

    finished product

--- recursion/product/product_start.ts
+++ recursion/product/product_start.ts
@@ -3,7 +3,9 @@ import { LLNodeNum } from "../common/ll";
 /** product: calculate the product of a linked list of numbers. */
 
 function product(nums: LLNodeNum | null): number {
-  return 42;
+  if(nums !== null && nums.next === null) return nums!.val;
+
+  return product(nums!.next) * nums!.val;
 }
 
 export { product };
