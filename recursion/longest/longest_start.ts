e6cb5810db21239244cba96f4cdb0a3e6a973a98
Brian Aston <astonb.dev@gmail.com>
2024-07-09 10:38:06 -0700

    finished longest

--- recursion/longest/longest_start.ts
+++ recursion/longest/longest_start.ts
@@ -3,7 +3,10 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  if(words === null) return -1;
+  let next = longest(words.next);
+
+  return Math.max(words.val.length, next);
 }
 
 export { longest };
