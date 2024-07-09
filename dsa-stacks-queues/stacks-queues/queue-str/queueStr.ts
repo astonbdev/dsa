7f0f757cd10fdcfb0da47efbf2cdf90e81090c0a
Brian Aston <astonb.dev@gmail.com>
2024-07-08 17:22:44 -0700

    Finished queues

--- dsa-stacks-queues/stacks-queues/queue-str/queueStr.ts
+++ dsa-stacks-queues/stacks-queues/queue-str/queueStr.ts
@@ -1,28 +1,32 @@
+import { LLStr } from "../common/ll";
 /** QueueStr: can remove from start or add to end */
 
 class QueueStr {
+  list: LLStr;
 
   constructor(initial: string[] = []) {
+    this.list = new LLStr(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    this.list.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    return this.list.shift();
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    return this.list.getAt(0)
   }
 
   /** isEmpty(): is the queue empty? */
   isEmpty(): boolean {
-    return false;
+    return this.list.length <= 0;
   }
 }
 
