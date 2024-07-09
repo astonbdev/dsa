0e53674e2560f3e0634b815ad35c789a51743ca6
Brian Aston <astonb.dev@gmail.com>
2024-07-09 10:44:51 -0700

    finished rev string

--- recursion/rev-string/revString_start.ts
+++ recursion/rev-string/revString_start.ts
@@ -1,7 +1,14 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+  //taking a bottom up approach
+  //break the string apart until we reach the end
+  //append the result to a new string that is the current str
+  //for above, you need to make sure to add them together in the right order
+
+  if(str === "") return "";
+
+  return revString(str.slice(0,1)) + str
 }
 
 export { revString };
