b56c1ab1bcc149c52e17fd709d7563e81bac4cd5
Brian Aston <astonb.dev@gmail.com>
2024-07-09 10:37:57 -0700

    finished every-other

--- recursion/every-other/everyOther_start.ts
+++ recursion/every-other/everyOther_start.ts
@@ -1,7 +1,25 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  function _getEveryOther(str:string, count: number): string{
+    if(str.length === 0) return str;
+    const characters = str.split("");
+    let result;
+
+    if(count % 2 !== 0){
+      characters.shift();
+    }
+
+    result = _getEveryOther(characters.join(""), count + 1);
+    return result; 
+  }
+  
+  return _getEveryOther(str, 0);
+
+  //BASE CASE: There is no more string
+  //start our new string
+  //append to it the result of getting the next string
+  //otherwise append an empty string
 }
 
 export { everyOther };
